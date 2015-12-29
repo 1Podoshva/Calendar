86a11d50deaa0abe930ab85577f07655b62b7b88
Александр Жадько <1flexon@gmail.com>
2015-12-29 18:28:30 +0300

    fix bug

--- Calendar/ViewController.m
+++ Calendar/ViewController.m
@@ -17,7 +17,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     TaskCalendar *calendar = [[[NSBundle mainBundle] loadNibNamed:@"TaskCalendar" owner:self options:nil] objectAtIndex:0];
     calendar.backgroundColor = [UIColor colorWithRed:34.0/255.0  green:31.0/255.0  blue:40.0/255.0  alpha:1];
     [calendar setFrame:CGRectMake(0, self.view.frame.size.height / 1.8, self.view.frame.size.width, self.view.frame.size.height / 2.2)];
@@ -25,12 +24,13 @@
     calendar.textColor = [UIColor colorWithRed:70.0/255.0  green:70.0/255.0  blue:79.0/255.0  alpha:1];
     calendar.textYearColor = [UIColor colorWithRed:51.0/255.0  green:48.0/255.0  blue:55.0/255.0  alpha:1];
     calendar.selectFastDaysColor = [UIColor colorWithRed:170.0/255.0  green:188.0/255.0  blue:98.0/255.0  alpha:1];
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> parent of 23dd402... new scrolling View
+    UILabel *view = [[UILabel alloc] initWithFrame:CGRectMake(50, 50 , 300, 300)];
+    [view setText:@"sdfsdfsdfsdf"];
+    [self.view addSubview:view];
+    
     [self.view addSubview:calendar];
+    
 }
 
 - (void)didReceiveMemoryWarning {
